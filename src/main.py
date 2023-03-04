4955ea1dd2ec2573cf93f2f9d3f649bfee15c17f
trevorhobenshield <trevorhobenshield@gmail.com>
2023-03-03 16:51:52 -0800

    configs

--- src/main.py
+++ src/main.py
@@ -1,59 +1,22 @@
+import asyncio
+import json
+import logging.config
 import random
 import re
 import sys
-import json
 import textwrap
 import time
-import logging.config
 from pathlib import Path
 from urllib.parse import quote, urlencode, parse_qs, urlsplit, urlunsplit
 
-import asyncio
 import aiohttp
 import requests
 
+from config import search_config, log_config
+
 reset = '\u001b[0m'
 colors = [f'\u001b[{i}m' for i in range(30, 38)]
-logging.config.dictConfig({
-    "version": 1,
-    "formatters": {
-        "simple": {
-            "format": "%(levelname)s: %(message)s"
-        }
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "level": "DEBUG",
-            "formatter": "simple",
-            "stream": "ext://sys.stdout"
-        },
-        "file": {
-            "class": "logging.FileHandler",
-            "level": "DEBUG",
-            "formatter": "simple",
-            "filename": "debug.log",
-            "encoding": "utf8",
-            "mode": "a"
-        }
-    },
-    "loggers": {
-        "myLogger": {
-            "level": "DEBUG",
-            "handlers": [
-                "console"
-            ],
-            "propagate": "no"
-        }
-    },
-    "root": {
-        "level": "DEBUG",
-        "handlers": [
-            "console",
-            "file"
-        ]
-    }
-})
+logging.config.dictConfig(log_config)
 logger = logging.getLogger(__name__)
 
 try:
@@ -66,7 +29,6 @@ except:
 if sys.platform != 'win32':
     try:
         import uvloop
-
         asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     except:
         ...
@@ -180,45 +142,7 @@ def main() -> int:
 
 
     '''))
-    search(*sys.argv[1:], config={
-        "include_profile_interstitial_type": 1,
-        "include_blocking": 1,
-        "include_blocked_by": 1,
-        "include_followed_by": 1,
-        "include_want_retweets": 1,
-        "include_mute_edge": 1,
-        "include_can_dm": 1,
-        "include_can_media_tag": 1,
-        "include_ext_has_nft_avatar": 1,
-        "include_ext_is_blue_verified": 1,
-        "include_ext_verified_type": 1,
-        "skip_status": 1,
-        "cards_platform": "Web-12",
-        "include_cards": 1,
-        "include_ext_alt_text": "true",
-        "include_ext_limited_action_results": "false",
-        "include_quote_count": "true",
-        "include_reply_count": 1,
-        "tweet_mode": "extended",
-        "include_ext_collab_control": "true",
-        "include_ext_views": "true",
-        "include_entities": "true",
-        "include_user_entities": "true",
-        "include_ext_media_color": "true",
-        "include_ext_media_availability": "true",
-        "include_ext_sensitive_media_warning": "true",
-        "include_ext_trusted_friends_metadata": "true",
-        "send_error_codes": "true",
-        "simple_quoted_tweet": "true",
-        "query_source": "typed_query",
-        "count": 100,
-        "q": "",
-        "requestContext": "launch",
-        "pc": 1,
-        "spelling_corrections": 1,
-        "include_ext_edit_control": "true",
-        "ext": "mediaStats,highlightedLabel,hasNftAvatar,voiceInfo,birdwatchPivot,enrichments,superFollowMetadata,unmentionInfo,editControl,collab_control,vibe"
-    })
+    search(*sys.argv[1:], config=search_config)
     return 0
 
 
